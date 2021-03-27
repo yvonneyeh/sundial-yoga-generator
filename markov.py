61896a6b88f3ea023a5310fbbf6b254f890cd040
Yvonne Yeh <code@yvonneyeh.com>
2021-03-27 10:46:06 -0700

    Reformat sample sequence

--- markov.py
+++ markov.py
@@ -1,6 +1,5 @@
 """Generate Markov chains from txt files."""
 
-import sys
 from random import choice
 
 
@@ -8,15 +7,66 @@ def open_and_read_file(filepath):
     """Take file path as string; return text as string.
     """
 
-    pose_list = []
     file = open(filepath)
     text = file.read()
     file.close()
 
+    return text
+
+
+def make_list(text):
+    """ Take the string of text, return a list of poses.
+    
+    First split the text to create a list of poses. 
+    
+    [cat, cow, downward dog, tree, etc ]
+    
+    
+    """
     poses = text_string.split(",")
+    poses.append(poses)
+    # To set a stop point, append None to the end of ou list.
+    poses.append(None)
 
-    return pose_list
+    return poses
+    
+
+def make_dict(pose_list):
+    """Take in list of poses, return dictionary of pose tuples and options of poses that follow them.
+    
+    Each key will be a tuple of two consecutive poses. 
+    
+    The value of a each will be a list containing every pose that follows the second tuple item.  
+    
+    pose_dict = {
+        (pose1, pose2): [option1, option2, option3],
+        (pose2, pose3): [option1]
+    }
+    """
+    chains = {}
+
+    # range over the array of poses, last pose excluded
+    for i in range(len(poses) - 2):
+        # key is a tuple of current pose plus the next pose
+        key = (poses[i], poses[i + 1])
+        # value = a list of the pose that is two poses after the current pose
+        value = poses[i + 2]
+
+        # if the tuple key isn't already in the dictionary, 
+        if key not in chains:
+            # create an empty list as the key's value
+            chains[key] = []
+
+        # append the value to the key
+        chains[key].append(value)
+
+        # or we could replace the last three lines with:
+        # chains.setdefault(key, []).append(value)
+
+    return chains
 
 
-def make_chains(pose_list):
-    """Take input text as string; return dictionary of Markov chains."""
+def make_text(chains):
+    """ Create sequence options """
+    
+    pass
